edb07782328416e35861488caaa256264991be1f
ThalesHAVarela <thales2004varela@gmail.com>
2024-11-19 17:15:13 -0300

    add backgrounds/alterações no css

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,37 @@ function App() {
       <Navbar />
       <main>
         <section id="Sobre">
-          <Box sx={{ backgroundImage: `url('/images/fotohalloweenThales.jpg')`,backgroundSize: 'cover',backgroundPosition: 'center', backgroundRepeat: 'no-repeat', display:'flex', height:"500px",marginBottom:2, justifyContent:"flex-start", alignItems:"flex-start",padding:"60px",margin:"20px",borderRadius:'10px'}}>
+          <Box sx={{ backgroundImage: `url('/images/fotohalloweenThales.jpg')`,backgroundSize: 'cover',backgroundPosition: 'center', display:'flex', height:"600px",marginBottom:2, justifyContent:"flex-start", alignItems:"flex-start",padding:"60px",margin:"20px",borderRadius:'10px'}}>
             <ul className='infolist'>
               <h2>Sobre a Include</h2>
-              <li>topico 1</li>
-              <li>topico 2</li>
-              <li>topico 3</li>
+              <li>Desde 2015, transformando ideias em inovação !</li>
+              <li>Soluções digitais e automação para impulsionar o seu negócio. Conecte-se ao futuro com a gente !</li>
+              <li>Na Include, nosso compromisso vai além de simplesmente desenvolver um projeto. 
+                Seguimos um processo cuidadoso e eficiente, desde a identificação das suas necessidades até o acompanhamento pós-entrega
+                grantindo que cada etapa seja pensada e executada com precisão.</li>
+                <div id='divinclude1'>
+                <ul>
+                  <li>Identificamos o problema</li>
+                  <li>Projetamos a solução ideal</li>
+                  <li>Desenvolvemos com excelência</li>
+                  <li>Entregamos com qualidade</li>
+                  <li>Acompanhamos e melhoramos continuamente</li>
+                </ul>
+                </div>
+              <li>
+                Na nossa empresa júnior, o primeiro passo para criar um projeto que realmente atenda às suas necessidades
+                é entender cada detalhe do que você precisa !
+              </li>
+              <li>
+                Quer saber mais como podemos ajudar sua empresa a crescer ? Entre em contato agora e vamos começar !
+              </li>
+              <a href="https://www.instagram.com/engenhariainclude?igsh=Ynl1Z3hwMXB2YTJv" target="_blank" rel="noopener">Entre em contato</a>
+
             </ul>
           </Box>
         </section>
         <section id="Candidatos">
-          <Box sx={{ bgcolor:"#373739",display:'flex', height:"500px",marginBottom:2, justifyContent:"center", alignItems:"center",borderRadius:'10px',padding:"10px",margin:"20px"}}>
+          <Box sx={{ backgroundImage: `url('/images/fotohalloween6.png')`,backgroundSize: 'cover',backgroundPosition: 'center',backgroundPositionY:'900px' ,display:'flex', height:"600px",marginBottom:2, justifyContent:"center", alignItems:"center",borderRadius:'10px',padding:"10px",margin:"20px"}}>
             <h7>hello world </h7>
           </Box>
         </section>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,10 @@ import './index.css'; // Arquivo de estilos para a Navbar
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">INCLUDE ENGENHARIA</div>
+      <div className="navbar-logo">
+        <h3 id='include1'>INCLUDE ENGENHARIA</h3>
+      <img src= '/images/fotoinclude.jpg' alt="fotoinclude" />
+      </div>
       <ul className="navbar-links">
         <li><a href="#Sobre">Sobre</a></li>
         <li><a href="#Candidatos">Candidatos</a></li>
